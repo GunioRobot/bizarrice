dbea55bf108b5fee2fe94e4cb75b49ef321588cc
Caio Romão <caioromao@gmail.com>
2009-10-03 15:22:43 -0300

    adding import_wrapper and moving externals to "lib"

--- main.py
+++ main.py
@@ -4,10 +4,7 @@ import config
 import os
 import sys
 
-# Force sys.path to have our own directory first, so we can import from it.
-sys.path.insert(0, config.APP_ROOT_DIR)
-sys.path.insert(1, os.path.join(config.APP_ROOT_DIR, 'externals'))
-
+import import_wrapper
 import wsgiref.handlers
 from google.appengine.ext import webapp
 from handlers import blog, admin, error
