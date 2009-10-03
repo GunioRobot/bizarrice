6a081eac2f37536295eb3e7843bb4d0b40ea3870
Caio Romão <caioromao@gmail.com>
2009-10-03 14:56:14 -0300

    configurations for blog.caioromao.com

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
+# -*- coding: utf-8 -*-
 import os
+
 
 APP_ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 SETTINGS = {
-    'title': 'Blog',
-    'description': "Google App Engine-Based Blog",
-    'author': 'John Doe',
-    'email': 'john@example.com',
-    'url': 'http://www.example.com',
+    'title': 'Caio Romão\'s Blog',
+    'description': 'No proper description set',
+    'author': 'Caio Romão',
+    'email': 'caioromao@gmail.com',
+    'url': 'http://blog.caioromao.com',
     'items_per_page': 10,
     # Enable/disable Google Analytics
     # Set to your tracking code (UA-xxxxxx-x), or False to disable
-    'google_analytics': False,
+    'google_analytics': 'UA-4613689-4',
     # Enable/disable Disqus-based commenting for posts
     # Set to your Disqus short name, or False to disable
-    'disqus': False,
+    'disqus': 'caio',
     # Enable/disable debug mode
     'debug': True,
 }
